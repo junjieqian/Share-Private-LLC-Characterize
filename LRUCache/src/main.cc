2326662fbb62efe7baf7c4a0b05fa9336be751f0
Junjie Qian <qjj1983@gmail.com>
2014-03-01 22:47:35 -0600

    main function update

--- LRUCache/src/main.cc
+++ LRUCache/src/main.cc
@@ -4,5 +4,20 @@
 
 #include "cache.h"
 
-// everything starts from here
+// process the command line options
+void doargs(int argc, char ** argv) {
 
+}
+
+// helper function
+int helper() {
+  cout << "USAGE of this tool....\n";
+  return 0;
+}
+
+// everything starts from here
+int main(int argc, char ** argv) {
+  if (argc<2) return helper();
+
+  doargs(argc, argv);
+}
